fc2bf1429f930b8d27f5d7e57929485ee2668c1a
Dorgenark <simonromainville@hotmail.com>
2018-04-04 18:36:52 +0200

    gallerie photo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-
-
-// smooth scroll
+// SMOOTH SCROLL
 $('a[href^="#top"]').click(function(){
 
 var scroll = $(this).attr("href");
@@ -10,3 +8,30 @@ var scroll = $(this).attr("href");
     }, 'slow');
 
 return false;});
+
+
+
+// IMAGE GALLERY
+$.fn.simpleSlides = function () {
+
+    return this.each(function () {
+        var simple_slideshow = $(this),
+            listItems = simple_slideshow.children('li'),
+            listLen = listItems.length,
+            i = 0,
+            changeList = function () {
+                listItems.eq(i).fadeOut(300, function () {
+                    i += 1;
+                    if (i === listLen) { i = 0; }
+                    listItems.eq(i).fadeIn(300);
+                });
+            };
+        listItems.not(':first').hide();
+        setInterval(changeList, 2000);
+    });
+}
+
+
+$(function(){
+    $('.slideshow').simpleSlides()
+});
